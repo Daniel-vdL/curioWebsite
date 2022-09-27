0fb8658e029eefc15cc301e70da651825db2aa53
Daniel-vdL <alexanderdanailin@gmail.com>
2022-09-27 10:35:10 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<body>
+  <header>
+    <div class="wrapper">
+      <div class="header-menu">
+    		<div class="logo">
+    			<img src="logos/curioPaars.png" alt="">	
+    		</div>
+    		<nav>
+    			<a href="index.php">Software Development</a>
+    			<a href="services.php">Lessen</a>
+                <a href="shop.php">Versnellen</a>
+    			<a href="contact.php">Blokken</a>
+				<a href="contact.php">Docenten</a>
+    		</nav>
+      </div>  
+   	</div>
+  </header>
+</body>
---
+++ openDagWebsite.php
@@ -0,0 +1,79 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>OpenDag</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php 
+require_once("header.php");
+?>
+<main>
+  <div class="wrapper">
+    <div class="opendag">
+        <div class="about">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci repellat earum, impedit laudantium consequuntur rem corporis dolorum perspiciatis nostrum repellendus, iste molestias deleniti quia praesentium similique. Laborum earum veniam tempore. Lorem ipsum dolor sit amet consectetur adipisicing, elit. Vitae, ducimus dolorum, dolores quas quos sunt, officiis quasi laborum quisquam autem doloribus ad? Eaque, praesentium! Veniam, corporis! Repellat dicta, illum optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolore voluptatem inventore expedita, cumque ab mollitia commodi facilis possimus modi maiores, blanditiis voluptas architecto necessitatibus! Culpa quod maiores eligendi esse!</p>
+          <img src="Fotos_locaties\Curio1.jpg" alt="">
+        </div>  
+        
+        <div class="reviews">
+          <h2>Info</h2>
+          <div class="review1">
+           <div class="text">
+              <h3>Software Development</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate quod, officia distinctio commodi deserunt reprehenderit molestiae saepe illum repellat, enim vitae atque cum ducimus. Voluptatum officia id unde sunt minus! Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Vel, eveniet eum, ut dicta vitae eos maxime amet sunt nam beatae minima rem illo, cumque adipisci pariatur fuga? Tempora, optio, excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ipsum nemo et quibusdam illo sunt eius nihil unde impedit expedita nesciunt, vitae odio aspernatur maiores praesentium? Fugiat accusamus repudiandae doloremque? </p>
+            </div> 
+            <img src="Mbo\examen.jpg" alt="picture test">
+          </div>
+          <div class="review2">
+            <div class="text">
+              <h3>Lessen</h3>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus non autem, nisi praesentium, earum nemo tenetur facilis alias animi. Alias voluptates similique error perferendis ad totam in consequuntur nostrum impedit. Lorem ipsum dolor sit amet consectetur adipisicing elit. A labore illum eos deserunt, dolorum, autem quidem sapiente error, rem dicta ducimus. Ad laudantium reprehenderit consequuntur minima optio culpa doloribus aspernatur!</p>
+            
+            </div>  
+  	      </div>
+          <div class="review3">
+            <div class="text">
+                <h3>Versnellen</h3>
+                <p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Assumenda itaque eum deleniti. Nisi illum quisquam deserunt corporis alias optio non facilis pariatur quibusdam veritatis voluptate similique, aut, asperiores fugit minus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores minima quisquam ipsum incidunt dignissimos blanditiis optio nesciunt autem vel saepe, earum, nisi, repellendus unde temporibus illum, maxime alias vitae reiciendis.</p>
+            </div>
+          </div>  
+          <div class="review4">
+          <img src="img\Opleidingsoverzicht3.png" alt="picture test">
+            <div class="text">
+              <h3>Blokken</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, similique libero adipisci atque quis suscipit. Repudiandae maiores distinctio velit facere, pariatur assumenda sint aliquam ex maxime, quisquam molestiae ratione corrupti!</p>
+            </div>
+          </div>  
+          <div class="review5">
+            <h3>Docenten</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit.</p>
+          </div>
+          <div class="smoelenboekje">
+            <div class="smoel">
+                <img src="img\teacher2.jpg" alt="">
+                <h3>Michel Meeuwesen</h3>
+            </div>
+            <div class="smoel">
+                <img src="img\new_teacher1.jpeg" alt="">
+                <h3>Ine Vermeer</h3>
+            </div>
+            <div class="smoel">
+                <img src="img\500x500.jpg" alt="">
+                <h3>Elton Boekhoudt</h3>
+            </div>
+            <div class="smoel">
+                <img src="img\leraar3.jpg" alt="">
+                <h3>Bart Roos</h3>
+            </div>
+       </div>   
+        </div>
+    </div>
+  </div>
+
+</main>
+</body>
+</html>
